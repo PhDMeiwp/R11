551a70585079a1072ee69c087833739eeb0a3bd9
PhDMeiwp <meiweipingg@163.com>
2018-05-22 11:20:29 +0900

    Source file update

--- R/data_summary.R
+++ R/data_summary.R
@@ -1,15 +1,28 @@
 #' Data Summary
 #' 
-#' @description The results include sumple number (N), mean, varicance (Var), standard deviation (sd), standard error (se, =sd/sqrt(n)), coefficient of variation (CV, %),
-#'  median, median absolute deviation (mad), the first quartile (Q1), the third quartile (Q3), minimum, maximum, corrected sum or squares (CSS), uncorrected sum or squares (USS), range, interquartile range (IQR).
+#' @description The results include sumple number (N), mean, varicance (Var), standard deviation (sd), standard error (se, = sd/sqrt(n)), coefficient of variation (CV, %),
+#'  median, median absolute deviation (mad), the first quartile (Q1), the third quartile (Q3), interquartile range (IQR, = Q3-Q1), minimum, maximum, range (Max-Min), corrected sum or squares (CSS), uncorrected sum or squares (USS).
 #' @import stats
 #' @export
 #' @examples 
 #' library(R11)
 #' x<-c(1,3,4,6,8,11,13,15,40,50,100) 
-#' data_summary(x)
+#' #' data_summary(x)
+#' 
+#' # Not run
+#' shapiro.test(x)  
+#' # if p-value>0.05 (i.e. normal distribution), use Mean +/- sd; 
+#' # else, use Meadian [Q1-Q3] or Median [IQR].
+#' 
+#' # Alternatively, mean (95% CI for the mean) and median (95%CI for the median) are also used in some scientific papers.
+#' # In general, 95%CI is not recommended, unless required by the target journal.
+#' library(DescTools)
+#' DescTools::MeanCI(x)
+#' DescTools::MedianCI(x)
+#' 
 #' @seealso \code{\link[stats]{IQR}}, \code{\link[stats]{mad}}, \code{\link[DescTools]{MeanCI}}, \code{\link[DescTools]{MedianCI}}, \code{\link[DescTools]{Skew}}, \code{\link[DescTools]{Kurt}}
-#' @note Find more useful functions in 'DescTools' pacakge, such as Gini Coefficient, Geometric Mean and Standard Deviation, G-Test for Count Data, Skewness and Kurtosis, Confidence Interval for the Mean, Confidence Interval for the Median.
+#' @note The basic functions in 'data_summary()' are cited from 'stats' package of R. \n Find more functions in 'DescTools' pacakge, such as Gini Coefficient, Geometric Mean and Standard Deviation, G-Test for Count Data, Skewness and Kurtosis, Confidence Interval for the Mean, Confidence Interval for the Median.
+#' @references citation("stats")
 
 data_summary <- function(x, digits = 3){
 options(digits = digits)
@@ -30,6 +43,19 @@ IQR <- IQR(x)
 mad <- mad(x)
 sm <- s/sqrt(n)
 
-data.frame(N=n, Mean=m, Var=v, sd=s, se=sm, CV=cv, Median=me, mad=mad, Q1=q1, Q3=q3, IQR=IQR, Min = min, Max = max,Range=R, 
- CSS=css, USS=uss, row.names="[1]")
+shapiro.test(x)->test
+print(test)
+if (test$p.value>=0.05){
+  message("\nThe data is Normal distribution (p-value >= 0.05). \nHence, it is recommended to use Mean +/- sd.\n\n")
+}
+else{message("\nThe data is non-Normal distribution (p-value < 0.05). \nHence, it is recommended to use Median (Q1-Q3).\n\n")
+}
+
+result1<-data.frame(N=n, Mean=m, Var=v, sd=s, se=sm, CV=cv, CSS=css, USS=uss, row.names="[1]")
+result2<-data.frame(Median=me, mad=mad, Q1=q1, Q3=q3, IQR=IQR, Min = min, Max = max,Range=R, 
+                   row.names="[2]")
+print(result1)
+cat("\n")
+print(result2)
+invisible(list(N=n, Mean=m, sd=s, se=sm, Median=me, Q1=q1, Q3=q3, IQR=IQR, Min = min, Max = max,Range=R, mad=mad, Var=v, CV=cv, CSS=css, USS=uss))
 }
--- README.md
+++ README.md
@@ -4,31 +4,36 @@
 
 ## Installing "R11" package in R
 
+Get the development version from github:
 
     install.packages("devtools")
 	devtools::install_github("PhDMeiwp/R11@master", force = TRUE)
 	library(R11)
 
+## usage
+	data_summary(x, digits = 3)
 
-## @Examples
+## Examples
 	
     x<-c(1,3,4,6,8,11,13,15,40,50,100) 
     data_summary(x, digits = 3)
 	
 then output as follows:
 
-	# data_summary(x)
-	     N Mean Var sd   se  CV  Median  mad  Q1   Q3   IQR   Min  Max   Range  CSS    USS
-	[1] 11 22.8 901 30 9.05 132    11   10.4  5   27.5  22.5   1   100     99   9014  14741
+ <img src="images/data_summary.JPG" width="400"/>
 
     
-## @description
+## description
 The results include sample number (N), mean, varicance (Var), 
 standard deviation (sd), standard error [se, =sd/sqrt(n)], coefficient of variation (CV, %),
 median, **median absolute deviation (mad)**, the first quartile (Q1), the third quartile (Q3), interquartile range (IQR), minimum, maximum, range
 corrected sum of squares (CSS), uncorrected sum of squares (USS).
 
-## @note 
-Find more useful functions in 'DescTools' pacakge, 
+## note 
+The basic functions in 'data_summary()' are cited from 'stats' package of R.
+
+Find more functions in 'DescTools' pacakge, 
 such as Gini Coefficient, Geometric Mean and Standard Deviation, G-Test for Count Data, **Skewness and Kurtosis, Confidence Interval for the Mean, Confidence Interval for the Median**.
 
+## references
+	citation("stats")
